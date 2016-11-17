92d3602ea28d641cf93de9dc3293c28c28cb73d5
Ryan Thrash <ryan@modx.com>
2016-11-17 14:13:03 -0600

    Initial pass at cleaning up the English language files

--- core/components/modmonitor/lexicon/en/default.inc.php
+++ core/components/modmonitor/lexicon/en/default.inc.php
@@ -1,23 +1,19 @@
 <?php
 
 $_lang['modmonitor'] = "modMonitor";
-$_lang['modmonitor_desc'] = "Monitoring Site Performance";
+$_lang['modmonitor_desc'] = "Monitor Site Performance";
 
-$_lang['modmonitor_requests_detailed'] = "Request to site";
-$_lang['modmonitor_requests_detailed_desc'] = "Detailed view";
+$_lang['modmonitor_requests_detailed'] = "Site Requests";
+$_lang['modmonitor_requests_detailed_desc'] = "Detailed View";
 
 $_lang['modmonitor_requests_items'] = "Elements";
-$_lang['modmonitor_requests_items_desc'] = "All caused elements";
+$_lang['modmonitor_requests_items_desc'] = "Affected Elements";
 
 $_lang['modmonitor.parent'] = 'Parent';
-$_lang['modmonitor.request_id'] = 'ID request';
+$_lang['modmonitor.request_id'] = 'Request ID';
 $_lang['modmonitor.element'] = 'Element';
-$_lang['modmonitor.name'] = 'Item name';
+$_lang['modmonitor.name'] = 'Item Name';
 $_lang['modmonitor.properties'] = 'Options';
-$_lang['modmonitor.time'] = 'Total Time';
-$_lang['modmonitor.db_queries'] = 'Queries to database';
-$_lang['modmonitor.db_queries_time'] = 'Time requests to database';
-$_lang['modmonitor.php_memory'] = 'Memory Used';
 $_lang['modmonitor.group_elements'] = 'Group elements';
 $_lang['modmonitor.err_request'] = 'Error executing query';
 $_lang['modmonitor.query'] = 'Address or ID. % - all characters, _ - one';
@@ -26,28 +22,30 @@ $_lang['modmonitor.time'] = 'Time';
 
 $_lang['modmonitor.from_cache'] = 'Cache';
 $_lang['modmonitor.from_cache_all'] = 'All';
-$_lang['modmonitor.from_cache_1'] = 'From cache';
-$_lang['modmonitor.from_cache_2'] = 'Not from cache';
+$_lang['modmonitor.from_cache_1'] = 'From Cache';
+$_lang['modmonitor.from_cache_2'] = 'Uncached';
 
 $_lang['modmonitor.context'] = 'Context';
 $_lang['modmonitor.php_error'] = 'Error code';
 $_lang['modmonitor.search'] = 'Search';
 $_lang['modmonitor.clear'] = 'Reset';
 $_lang['modmonitor.truncate_stat'] = 'Delete';
-$_lang['modmonitor.truncate_confirm'] = 'Delete all the statistics? It irrevocably.';
+$_lang['modmonitor.truncate_confirm'] = 'Permanently delete all statistics? This cannot be undone.';
 $_lang['modmonitor.context_key'] = 'Context';
 $_lang['modmonitor.url'] = 'Address';
-$_lang['modmonitor.auth_from_name'] = 'Auth from name ';
+$_lang['modmonitor.auth_from_name'] = 'Authenticate by ';
 $_lang['modmonitor.date'] = 'Date';
 $_lang['modmonitor.resource_id'] = 'Document ID';
 $_lang['modmonitor.user_id'] = 'User';
-$_lang['modmonitor.http_status'] = 'Response code';
-$_lang['modmonitor.php_error'] = 'php error';
-$_lang['modmonitor.time'] = 'Execution time';
-$_lang['modmonitor.php_memory'] = 'Used memory MB';
-$_lang['modmonitor.db_queries'] = 'Queries to database';
-$_lang['modmonitor.db_queries_time'] = 'Time requests to database';
+$_lang['modmonitor.http_status'] = 'Response Code';
+$_lang['modmonitor.php_error'] = 'PHP Error';
+$_lang['modmonitor.time'] = 'Execution Time';
+$_lang['modmonitor.php_memory'] = 'Memory Used (MB)';
+$_lang['modmonitor.db_queries'] = 'Database Queries';
+$_lang['modmonitor.db_queries_time'] = 'Time for Queries';
+$_lang['modmonitor.time'] = 'Total Time';
 $_lang['modmonitor.unable_parse_data'] = 'Failed to parse data';
+
 $_lang['modmonitor.err_descr'] = 'Error description';
 $_lang['modmonitor.err_descr_msg'] = 'Error';
 $_lang['modmonitor.err_descr_file'] = 'File';
@@ -55,6 +53,5 @@ $_lang['modmonitor.err_descr_line'] = 'Line';
 
 $_lang['modmonitor.clear_data_success'] = 'Data has been successfully cleared';
 
-
 $_lang['modmonitor.search_statistics'] = 'Query statistics';
-$_lang['modmonitor.called_elements'] = 'invoked elements';
+$_lang['modmonitor.called_elements'] = 'Elements used';
